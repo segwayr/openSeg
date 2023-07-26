8a1120b0aef639afc3d68d1ca70bc1c217cecb0d
segwayr <112672693+segwayr@users.noreply.github.com>
2023-07-26 09:50:48 +0900

    Update curCap.ps1

--- curCap.ps1
+++ curCap.ps1
@@ -8,8 +8,8 @@ $TimerTick= {
     $position = [System.Windows.Forms.Cursor]::Position
     $graphics.CopyFromScreen($position. X* $dpi, $position. Y * $dpi, 0, 0, $bitmap.Size)
     $pixel = $bitmap.GetPixel(0, 0)
-    $decValue = "RGB([0], [1], [2])" -f $pixel.R, $pixel.G, $pixel.B
-    $label.Text = "X:" + $position.X + "Y:" + $position. Y + $decValue
+    $decValue = "RGB({0}, {1}, {2})" -f $pixel.R, $pixel.G, $pixel.B
+    $label.Text = "X:" + $position.X + "Y:" + $position.Y + "`n" + $decValue
 }
 
 
@@ -27,18 +27,18 @@ $label.Location = New-Object System.Drawing.Point (10, 10)
 $label.Size = New-Object System.Drawing.Size(160, 40)
 $label.Font = New-Object System.Drawing.Font("ＭＳ ゴシック", 10)
 
-#ブルダウン
+#プルダウン
 $pullLabel = New-Object System.Windows.Forms.Labele
 $pullLabel.Location = New-Object System.Drawing.Point(10,55)
 $pullLabel.Size = New-Object System.Drawing.Size(50,10)
 $pullLabel.Font = New-Object System.Drawing.Font("ＭＳ ゴシック", 8)
-$pullLabel.text = “画面倍率”
+$pullLabel.text = "画面倍率"
 
 $dpiScaling = New-Object System.Windows.Forms.Combobox
 $dpiScaling.Location = New-Object System.Drawing.Point (10,70)
 $dpiScaling.size = New-Object System.Drawing.Size(50,30)
 $dpiScaling.DropDownStyle = "DropDown"
-$dpiScaling.FlatStyle= "standard"
+$dpiScaling.FlatStyle = "standard"
 $dpiScaling.font = $Font
 $dpiScaling.text = "125%"
 
