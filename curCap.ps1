03a2ffc72ad17722067a12d33352104fbf80f7b8
segwayr <112672693+segwayr@users.noreply.github.com>
2025-08-26 17:51:07 +0900

    Update curCap.ps1
    よく見たらすっかり拡大/縮小の座標計算書き忘れてたので書き足した
    またデフォルト値をメインディスプレイの設定情報をそのまま代入する形にした

--- curCap.ps1
+++ curCap.ps1
@@ -5,14 +5,16 @@ Add-Type -AssemblyName System.Drawing, System.Windows.Forms
 $bitmap = New-Object System.Drawing.Bitmap(1, 1)
 $graphics = [System.Drawing.Graphics]::FromImage($bitmap)
 $Timer = New-Object System.Windows.Forms.Timer
+$dpiX = [Systen.Windows.Form.Screen]::PrimaryScreen.Bounds.Width / [System.Windows.Forms.Screen]::PrimaryScreen.WorkingArea.Width
+$scalePercent = [Math]::Round($dpiX * 100)
 
 $TimerTick= {
-    $dpi = [int]$dpiScaling.text.Substring(0, 3) * 0.01
+    $dpi = [Math]::Round([int]$dpiScaling.text.Substring(0, 3) * 0.01)
     $position = [System.Windows.Forms.Cursor]::Position
-    $graphics.CopyFromScreen($position. X* $dpi, $position. Y * $dpi, 0, 0, $bitmap.Size)
+    $graphics.CopyFromScreen($position. X * $dpi, $position. Y * $dpi, 0, 0, $bitmap.Size)
     $pixel = $bitmap.GetPixel(0, 0)
     $decValue = "RGB({0}, {1}, {2})" -f $pixel.R, $pixel.G, $pixel.B
-    $label.Text = "X:" + $position.X + "Y:" + $position.Y + "`n" + $decValue
+    $label.Text = "X:" + $position.X * $dpi + "Y:" + $position.Y * $dpi + "`n" + $decValue
 }
 
 
@@ -33,17 +35,17 @@ $label.Font = New-Object System.Drawing.Font("ＭＳ ゴシック", 10)
 #プルダウン
 $pullLabel = New-Object System.Windows.Forms.Label
 $pullLabel.Location = New-Object System.Drawing.Point(10,55)
-$pullLabel.Size = New-Object System.Drawing.Size(50,10)
+$pullLabel.Size = New-Object System.Drawing.Size(70,20)
 $pullLabel.Font = New-Object System.Drawing.Font("ＭＳ ゴシック", 8)
-$pullLabel.text = "画面倍率"
+$pullLabel.text = "拡大/縮小"
 
 $dpiScaling = New-Object System.Windows.Forms.Combobox
-$dpiScaling.Location = New-Object System.Drawing.Point (10,70)
+$dpiScaling.Location = New-Object System.Drawing.Point (10,75)
 $dpiScaling.size = New-Object System.Drawing.Size(50,30)
 $dpiScaling.DropDownStyle = "DropDown"
 $dpiScaling.FlatStyle = "standard"
 $dpiScaling.font = $Font
-$dpiScaling.text = "125%"
+$dpiScaling.text = "$scalePercent%"
 
 [void] $dpiScaling.Items.Add("100%")
 [void] $dpiScaling.Items.Add("125%")
